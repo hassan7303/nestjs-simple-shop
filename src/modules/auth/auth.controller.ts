454d97eb892099bbc0f11a9fd3dde12ed8cc05d5
hsnali <hassanali7303@gmail.com>
2025-02-07 14:15:09 +0330

    feat: implement registration and login endpoints in AuthController

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from '../users/dto/create-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() body: CreateUserDto) {
+    
+    return this.authService.register(
+        body.firstName, 
+        body.lastName, 
+        body.phoneNumber, 
+        body.email, 
+        body.password,
+        body.profilePicture,
+        body.role
+    );
+  }
+
+  @Post('login')
+  async login(@Body() body: { username: string; password: string }) {
+    return this.authService.login(body.username, body.password);
+  }
+}
