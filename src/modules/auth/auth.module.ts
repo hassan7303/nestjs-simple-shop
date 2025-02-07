9d52154e557c75b7113e2a8c0525e8abbb1d4206
hsnali <hassanali7303@gmail.com>
2025-02-07 12:32:52 +0330

    feat: integrate MikroORM with AuthModule for User entity management

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { User } from '../users/user.entity';
 
 @Module({
+  imports: [MikroOrmModule.forFeature([User])],
+  providers: [AuthService],
   controllers: [AuthController],
-  providers: [AuthService]
 })
 export class AuthModule {}
