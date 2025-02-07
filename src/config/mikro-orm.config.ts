cae1637bb20d78b2e321bd214206d844d614936c
hsnali <hassanali7303@gmail.com>
2025-02-07 14:15:04 +0330

    fix: update MongoDB client URL to include authSource parameter

--- src/config/mikro-orm.config.ts
+++ src/config/mikro-orm.config.ts
@@ -3,7 +3,7 @@ import { MongoDriver } from '@mikro-orm/mongodb';
 
 const config: MikroOrmModuleOptions = {
   driver: MongoDriver, // استفاده از درایور اختصاصی MongoDB
-  clientUrl: process.env.MONGODB_URI || 'mongodb://root:123@localhost:27017/shop',
+  clientUrl: process.env.MONGODB_URI || 'mongodb://root:123@localhost:27017/shop?authSource=admin',
   dbName: 'shop',
   entities: ['./dist/modules/**/*.entity.js'],
   entitiesTs: ['./src/modules/**/*.entity.ts'],
